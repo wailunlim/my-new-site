df40a8d0c1831afec5f9483af6f1a150672bc18b
Wai Lun <wailun@u.nus.edu>
2020-03-08 22:45:24 +0100

    Add basic information

---
+++ components/About/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full px-5">
+    <p class="text-6xl font-medium">
+      <slot name="title" />
+    </p>
+    <p class="text-4xl font-medium mb-5">
+      <slot name="subtitle" />
+    </p>
+    <p class="text-2xl">
+      <slot name="body" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="container mx-auto mt-5">
-    <h1>Hello world!</h1>
+    <info>
+      <template v-slot:title>
+        Hello.
+      </template>
+      <template v-slot:subtitle>
+        My name is Wai Lun.
+      </template>
+      <template v-slot:body>
+        <p>
+          I am a current third-year Computer Science student at the National University of Singapore. I am passionate about technology and I hope to one day contribute positively to the world through code.
+        </p>
+        <br>
+        <p>
+          I am currently in Munich, Germany for an internship with Agrando as a frontend developer. There I hope to learn more about businesses on top of improving my technical skills. I also hope to improve my German and find time to travel around Europe.
+        </p>
+        <br>
+        <p>
+          My main interests are in the field of programming languages and software engineering. Outside of programming I enjoy video games, TV shows and movies. I also play football and go for the occasional swim to keep fit.
+        </p>
+      </template>
+    </info>
   </div>
 </template>
+
+<script>
+import Info from '../components/About/Info'
+
+export default {
+  components: { Info }
+}
+</script>
