2a57be970a86624e943c520b9d09baa2726e09e5
Wai Lun <wailun@u.nus.edu>
2020-03-08 22:21:28 +0100

    Extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'vue-mint': '#41B883'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
