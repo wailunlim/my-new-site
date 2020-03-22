93a8af380f947e707783111cb1a6ba01bd3729c7
Wai Lun <wailun@u.nus.edu>
2020-03-22 17:24:48 +0100

    Do not nest block-level elements in <p>

--- pages/index.vue
+++ pages/index.vue
@@ -8,17 +8,11 @@
         My name is Wai Lun.
       </template>
       <template v-slot:body>
-        <p>
-          I am a current third-year Computer Science student at the National University of Singapore. I am passionate about technology and I hope to one day contribute positively to the world through code.
-        </p>
-        <br>
-        <p>
-          I am currently in Munich, Germany for an internship with Agrando as a frontend developer. There I hope to learn more about businesses on top of improving my technical skills. I also hope to improve my German and find time to travel around Europe.
-        </p>
-        <br>
-        <p>
-          My main interests are in the field of programming languages and software engineering (especially web development). Outside of programming I enjoy video games, TV shows and movies. I also play football and go for the occasional swim to keep fit.
-        </p>
+        I am a current third-year Computer Science student at the National University of Singapore. I am passionate about technology and I hope to one day contribute positively to the world through code.
+        <br><br>
+        I am currently in Munich, Germany for an internship with Agrando as a frontend developer. There I hope to learn more about businesses on top of improving my technical skills. I also hope to improve my German and find time to travel around Europe.
+        <br><br>
+        My main interests are in the field of programming languages and software engineering (especially web development). Outside of programming I enjoy video games, TV shows and movies. I also play football and go for the occasional swim to keep fit.
       </template>
     </info>
   </div>
