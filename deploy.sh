000aaebfe9b9109d31dd1b195512a2cc4f33f252
Wai Lun <wailun@u.nus.edu>
2020-03-22 21:12:34 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into build output directory
+cd dist
+
+# git
+git init
+git add .
+git commit -m 'Deploy'
+git push -f git@github.com:wailunlim/wailunlim.github.io.git master
+
+cd -
