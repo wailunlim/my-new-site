33c72276eaf52ad3ff054a4d8f1c0d4d0a7897a9
Wai Lun <wailun@u.nus.edu>
2020-03-22 18:02:28 +0100

    Update about

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       <template v-slot:body>
         I am a current third-year Computer Science student at the National University of Singapore. I am passionate about technology and I hope to one day contribute positively to the world through code.
         <br><br>
-        I am currently in Munich, Germany for an internship with Agrando as a frontend developer. There I hope to learn more about businesses on top of improving my technical skills. I also hope to improve my German and find time to travel around Europe.
+        I am currently interning at <i>Agrando</i> as a frontend developer. There I hope to learn more about startups & businesses on top of improving my technical skills.
         <br><br>
         My main interests are in the field of programming languages and software engineering (especially web development). Outside of programming I enjoy video games, TV shows and movies. I also play football and go for the occasional swim to keep fit.
       </template>
