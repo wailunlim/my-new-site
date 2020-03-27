b9d4db633a57763123d73d5507b81ac5cb2be2d8
Wai Lun <wailun@u.nus.edu>
2020-03-27 22:26:12 +0100

    Add blog post page

---
+++ components/blog/BlogPost/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <banner-title class="text-bold">
+      {{ post.title }}
+    </banner-title>
+    <!-- eslint-disable-next-line -->
+    <div v-html="post.body" />
+  </div>
+</template>
+
+<script>
+import BannerTitle from '@/components/common/BannerTitle'
+
+export default {
+  components: { BannerTitle },
+
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,27 @@
+<template>
+  <blog-post :post="post" />
+</template>
+
+<script>
+import { butter } from '@/buttercms.js'
+import BlogPost from '@/components/blog/BlogPost'
+
+export default {
+  components: { BlogPost },
+
+  async asyncData ({ route }) {
+    try {
+      const { data } = await butter.post.retrieve(route.params.slug)
+      return { post: data.data }
+    } catch (e) {
+      return {}
+    }
+  },
+
+  data () {
+    return {
+      post: null
+    }
+  }
+}
+</script>
